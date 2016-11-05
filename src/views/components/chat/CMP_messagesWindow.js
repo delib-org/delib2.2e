b33d6505f0da8c8ab6941280be08bc639c805948
Eilon Mor <eilonmore13@gmail.com>
2016-11-05 14:35:43 +0200

    messages window styling

--- src/views/components/chat/CMP_messagesWindow.js
+++ src/views/components/chat/CMP_messagesWindow.js
@@ -4,43 +4,95 @@ class MessagesWindow extends Component {
   render() {
     return (
       <div>
-        <div className ="row">
+
+        <div className ="row message-row">
           <div className="col s4">
-            <div className="card-panel red lighten-5 z-depth-1">
-              <span className="black-text">
-                This is a square image. Add the "circle" class to it to make it appear circular.
-              </span>
+            <div className="card-panel message-bubble left-message-bubble red lighten-2 z-depth-2 ">
+              <div className = "row">
+                <span className="col s12 left-align black-text">
+                  אילון מור
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 left-align white-text">
+                  היי, זוהי הודעה ראשונה בדליב-ריאקט צאט!
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 right-align black-text">
+                  13:28
+                </span>
+              </div>
             </div>
           </div>
         </div>
-        <div className ="row">
+
+        <div className ="row message-row">
           <div className="col s4">
-            <div className="card-panel red lighten-5 z-depth-1">
-              <span className="black-text">
-                This is a square image. Add the "circle" class to it to make it appear circular.
-              </span>
+            <div className="card-panel message-bubble left-message-bubble red lighten-2 z-depth-2 ">
+              <div className = "row">
+                <span className="col s12 left-align black-text">
+                  אילון מור
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 left-align white-text">
+                  היי, זוהי הודעה ראשונה בדליב-ריאקט צאט!
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 right-align black-text">
+                  13:28
+                </span>
+              </div>
             </div>
           </div>
         </div>
-        <div className ="row">
+
+        <div className ="row message-row">
           <div className="col s4">
-            <div className="card-panel red lighten-5 z-depth-1">
-              <span className="black-text">
-                This is a square image. Add the "circle" class to it to make it appear circular.
-              </span>
+            <div className="card-panel message-bubble left-message-bubble red lighten-2 z-depth-2 ">
+              <div className = "row">
+                <span className="col s12 left-align black-text">
+                  אילון מור
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 left-align white-text">
+                  היי, זוהי הודעה ראשונה בדליב-ריאקט צאט!
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 right-align black-text">
+                  13:28
+                </span>
+              </div>
             </div>
           </div>
         </div>
-    
-        <div className ="row">
+
+        <div className ="row message-row">
           <div className="col s4 push-s8">
-            <div className="card-panel red lighten-5 z-depth-1">
-              <span className="black-text">
-                This is a square image. Add the "circle" class to it to make it appear circular.
-              </span>
+            <div className="card-panel message-bubble right-message-bubble red lighten-2 z-depth-2 ">
+              <div className = "row">
+                <span className="col s12 left-align black-text">
+                  אילון מור
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 left-align white-text">
+                  היי, זוהי הודעה ראשונה בדליב-ריאקט צאט!
+                </span>
+              </div>
+              <div className = "row">
+                <span className="col s12 right-align black-text">
+                  13:28
+                </span>
+              </div>
             </div>
           </div>
         </div>
+
       </div>
     )
   }
