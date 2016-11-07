d1c4168f35cf5cc232057fafed471417c83a7a0c
Hai Dvash <tald123@gmail.com>
2016-11-07 18:17:55 +0200

    temp

--- src/core/reducers/R_chatRoom.js
+++ src/core/reducers/R_chatRoom.js
@@ -1,3 +1,16 @@
-/**
- * Created by kido on 04/11/16.
- */
+import initialState from './initialState';
+import {
+    UPDATE_GROUP,
+    CLEAN_GROUPS
+} from '../../constants';
+
+export default function(state = initialState.groups, action) {
+    switch(action.type) {
+        case UPDATE_GROUP:
+            return {...state, ...action.payload};
+        case CLEAN_GROUPS:
+            return {};
+        default:
+            return state;
+    }
+}
