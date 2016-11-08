35f8b4ec27561681fe8b4298a0f87f8ac306059e
tzachi.h <tzachi.h@convertmedia.com>
2016-11-08 09:51:17 +0200

    Draft.js editor feature

--- src/index.js
+++ src/index.js
@@ -9,6 +9,53 @@ import reducers from './core/reducers';
 import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
 
+//test
+
+import {Editor, EditorState, RichUtils} from 'draft-js';
+
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+    _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+    _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+
+render() {
+    return (
+      <div>
+        <div className="DraftEditor-toolbar">
+            <button onClick={this._onBoldClick.bind(this)}>מודגש</button>
+            <button onClick={this._onItalicClick.bind(this)}>נטוי</button>
+        </div>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
+}
+
+//test
+
 // const createStoreWithMiddleware = applyMiddleware(ReduxThunk, promiseMiddleware)(createStore);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -18,6 +65,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
+     <MyEditor />
   </Provider>
   , document.querySelector('.container'));
